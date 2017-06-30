16db65874f7245264ef07038920c6de3b0e0a467
Grzegorz Jozef Jaworski <webmaster@grzegorzjaworski.de>
2017-06-30 09:58:36 +0200

    db backup

--- database/db_buckup/29_06_17.sql
+++ database/db_buckup/29_06_17.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `saxer` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `saxer`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: saxer
@@ -264,4 +266,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-30  9:41:17
+-- Dump completed on 2017-06-30  9:58:07
